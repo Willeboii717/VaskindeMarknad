35418cdba762e51a2e7905475f3dbb99cdfa2e28
Willeboii717 <willeboii@jobbig.se>
2023-11-06 12:17:03 +0100

    Backend small fixes createUser broken WIP

--- backend/src/controllers/CustomerController.ts
+++ backend/src/controllers/CustomerController.ts
@@ -1,15 +1,18 @@
-//TODO, flytta skit
+//TODO, flytta saker hit
 import Express, { NextFunction, Request, Response } from "express";
 import { executeQuery } from "../databaseHandler/db.query";
 import { CustomerModel } from "../models/customer";
 
 export const checkIfUserExists = async (req: Request, res: Response, next:NextFunction) => {
-    const customer:CustomerModel = req.body; //Adds body to customer interface
-
-    const query = "SELECT * FROM CUSTOMERS WHERE USERNAME = ?"; //Defining query to check if username already exists, TODO, add more checks
-    const result:CustomerModel = await executeQuery(query, [customer.username]); //Async execution of query
+    var customer:CustomerModel = req.body; //Adds body to customer interface
+    console.log("Checkpoint 1");
     
-    if ( result.username != null || result.username != undefined) { // Checks if returned username is null or undefined I.E doesnt exist
+
+    const query = "SELECT * FROM CUSTOMERS WHERE username = ?"; //Defining query to check if username already exists, TODO, add more checks
+    customer = await executeQuery(query, [customer.username]); //Async execution of query
+    console.log("Checkpoint 2");
+    
+    if ( customer.username != null || customer.username != undefined) { // Checks if returned username is null or undefined I.E doesnt exist
         console.log("[server]: User already exists");
         res.status(409).send({
             error: {
--- backend/src/routers/customersRouter.ts
+++ backend/src/routers/customersRouter.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response} from "express";
+import express, {NextFunction, Request, Response} from "express";
 const router = express.Router();
 //Database
 import { executeQuery } from "../databaseHandler/db.query";
@@ -12,77 +12,46 @@ import { runValidator } from "../validators/validatorRunner";
 import { checkIfUserExists } from "../controllers/CustomerController";
 
 
-
-
-// GET: Retrieve a list of all customers
-router.get('/getCustomers', async (req, res) => {
-  const query = await executeQuery("SELECT * FROM customers");
-
-  const Customer:CustomerModel[] = query;
-  if (Customer.length > 0) {
-    res.send(Customer);
-  }
-  else {
-    console.log("Error, could not get Customers")
-  }
-});
-
-//Get specific customer by ID
-router.get("/customers/:id", async (req, res) => {
-  const customerId = req.params.id;
-  const query = await executeQuery
-    ("SELECT * FROM customers WHERE id = ?", [customerId]);  
-  const customer: CustomerModel = query;
-
-  if ( customer.id != null ) {
-    res.send(customer);
-  }
-  else {
-    res.status(404).send("Error, customer with that ID is not found");  
-  }
-});
-
-
-
-router.post('/createCustomer',
-  createCustomerValidator, //determines what should be validated
+router.post('/createUser',
+  createCustomerValidator, // determines what should be validated
   runValidator, // runs validator on body
-  checkIfUserExists, //Checks if users exists
+  checkIfUserExists, // Checks if users exist
   async (req: Request, res: Response) => {
     console.log("[server]: entering Post, createCustomer");
-  //Placing body in customer interface
-  const customer:CustomerModel = req.body;
-  try {
-    // Defining query to insert a customer
+    // Placing body in User interface
+    const user: CustomerModel = req.body;
     const query = 'INSERT INTO customers (username, email, firstname, lastname, password) VALUES (?, ?, ?, ?, ?)';
-    // Execute the query with parameters
-    await executeQuery(query, [customer.username, customer.email, customer.firstname, customer.lastname, customer.password]);
+    
+    executeQuery(query, [user.username, user.email, user.firstname, user.lastname, user.password])
+      .then((result) => {
+        console.log(result);
+        res.send("User created successfully"); //This is a WIP, need to split DB query, so this doesnt expect a return value
+      })
+      .catch((error) => {
+        if (error === "NO_DATA") {
+          console.log(error);
+          res.send("NO DATA ERROR");
+        }
+        else {
+          console.log(error);
+          res.send("UNSPECIFIED ERROR");
+        }
+      });
+  });
 
-    // If the insertion is successful, return a success message
-    res.status(201).send({msg: "Customer Created Successfully"});
-    console.log("[server]: Post Success, Customer Created");
-  } 
-  // Handle any database errors
-  catch (error) {
-    res.status(500).send({msg: "Error creating customer"});
-    console.log("[server]: Post Failure, Database Error");
-  }
-});
 
 
-router.post('/loginCustomer',
+router.post('/loginUser',
   loginValidator,
   runValidator,
   async (req: Request, res: Response) => {
     console.log("[server]: entering Post, loginCustomer ", req.body);
-    const frontEndDataCustomer:loginCredentialModel = req.body;
+    const frontEndDataUser:loginCredentialModel = req.body;
     try {
       //Query definition
       const query = 'SELECT * FROM CUSTOMERS WHERE USERNAME = ? AND PASSWORD = ?';
       //Exe Query
-      const resultOfQuery:CustomerModel = await executeQuery(query, [frontEndDataCustomer.username, frontEndDataCustomer.password])
-      console.log(resultOfQuery);
-      
+      const resultOfQuery:CustomerModel = await executeQuery(query, [frontEndDataUser.username, frontEndDataUser.password])
       //If successful find of customer, return
       if (resultOfQuery != null) {
         res.status(201).send({msg: "Customer Authenticated"});
@@ -91,8 +60,6 @@ router.post('/loginCustomer',
     }    
     // Handle any database errors
     catch (error) {
-      console.log(error);
-      
       if (error === "NO_DATA") {
         const resError: httpErrorModel = {
           code: 'CREDENTIALS_MISMATCHED',
